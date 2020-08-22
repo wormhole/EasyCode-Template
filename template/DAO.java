5e7e766e8d28b746cb00e2fd0d1a6fb6296cc8fc
wormhole <363408268@qq.com>
2020-08-22 16:41:58 +0800

    注释

--- template/DAO.java
+++ template/DAO.java
@@ -22,25 +22,91 @@ import java.util.Map;
 @Repository
 public interface $!{tableName} {
     
+    /**
+     * 根据条件统计
+     *
+     * @param wrapper
+     * @return
+     */
     int countByCondition(QueryWrapper wrapper);
 
+    /**
+     * 根据id查询
+     *
+     * @param id
+     * @return
+     */
     $!{tableInfo.name} select(String id);
     
+    /**
+     * 条件查询
+     *
+     * @param wrapper
+     * @return
+     */
     List<$!{tableInfo.name}> selectByCondition(QueryWrapper wrapper);
 
+    /**
+     * 新增
+     *
+     * @param permission
+     * @return
+     */
     int insert($!{tableInfo.name} $!tool.firstLowerCase($!{tableInfo.name}));
 
+    /**
+     * 批量新增
+     *
+     * @param permissions
+     * @return
+     */
     int batchInsert(List<$!{tableInfo.name}> $!tool.firstLowerCase($!{tableInfo.name})s);
 
+    /**
+     * 删除
+     *
+     * @param id
+     * @return
+     */
     int delete(String id);
 
+    /**
+     * 批量删除
+     *
+     * @param ids
+     * @return
+     */
     int batchDelete(List<String> ids);
     
+    /**
+     * 根据条件删除
+     *
+     * @param wrapper
+     * @return
+     */
     int deleteByCondition(QueryWrapper wrapper);
 
+    /**
+     * 更新
+     *
+     * @param permission
+     * @return
+     */
     int update($!{tableInfo.name} $!tool.firstLowerCase($!{tableInfo.name}));
 
+    /**
+     * 批量更新
+     *
+     * @param permissions
+     * @return
+     */
     int batchUpdate(List<$!{tableInfo.name}> $!tool.firstLowerCase($!{tableInfo.name})s);
     
+    /**
+     * 根据条件更新
+     *
+     * @param wrapper
+     * @return
+     */
     int updateByCondition(QueryWrapper wrapper);
 }
